4603afa0ff05b6f9147b9d68c7629661dca6291a
Eirik Lied <eiriklied@gmail.com>
2012-09-20 18:44:13 +0200

    remove test on color for p

--- public/cactus_spec/typography_spec.js
+++ public/cactus_spec/typography_spec.js
@@ -6,5 +6,4 @@ Cactus.expect("h1", "color").toHaveColor("#4D4F53");
 // paragraphs
 Cactus.expect("p", "font-family").toContain("Arial");
 Cactus.expect("p", "font-size").toEqual("16px");
-Cactus.expect("p:not([class])", "color").toHaveColor("#4d4f53");
 
